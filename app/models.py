94a731483ac664e0e7b62f23d2aec8d97a83623a
santhoshk-github <santhoshmecmca2013@gmail.com>
2024-08-02 19:12:44 +0530

    Update models.py
    new fields added in models script

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
+# usr/bin/python
+from sqlalchemy import Column, Integer, String,Date
+from database import Base
 
+### Patient table class
 class Patient(Base):
     __tablename__ = "patients"
     
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50))
     age = Column(Integer)
+    date_of_birth = Column(String(10))
     medical_history = Column(String(250))
-
+    lab_results = Column(String(250))
