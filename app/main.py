b6f1a2d04062c27003417ab0c877119cf67fc2f0
santhoshk-github <santhoshmecmca2013@gmail.com>
2024-08-02 19:13:47 +0530

    Update main.py
    updated main script.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
-
+# usr/bin/python
+from fastapi import FastAPI
+from fastapi import Depends
 from sqlalchemy.orm import Session
-from . import models, schemas, crud
-from .database import SessionLocal, engine
+import models, schemas, crud
+from database import SessionLocal, engine
 
-
-# Create the database tables
+### Create the database tables
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency to get the database session
+### Dependency to get the database session
 def get_db():
     db = SessionLocal()
     try:
@@ -17,21 +18,25 @@ def get_db():
     finally:
         db.close()
 
+### Create patient detail
 @app.post("/patients/", response_model=schemas.Patient)
 def create_patient(patient: schemas.PatientCreate, db: Session = Depends(get_db)):
     return crud.create_patient(db=db, patient=patient)
 
+### Read patient detail
 @app.get("/patients/{patient_id}", response_model=schemas.Patient)
 def read_patient(patient_id: int, db: Session = Depends(get_db)):
     db_patient = crud.get_patient(db, patient_id=patient_id)
     if db_patient is None:
-        raise HTTPException(status_code=404, detail="Patient not found")
+        raise Exception(status_code=404, detail="Patient not found")
     return db_patient
 
+### Update patient detail
 @app.put("/patients/{patient_id}", response_model=schemas.Patient)
 def update_patient(patient_id: int, patient: schemas.PatientCreate, db: Session = Depends(get_db)):
     return crud.update_patient(db=db, patient_id=patient_id, patient=patient)
 
+### Delete patient detail
 @app.delete("/patients/{patient_id}")
 def delete_patient(patient_id: int, db: Session = Depends(get_db)):
     return crud.delete_patient(db=db, patient_id=patient_id)
