a83f60414cd4d5a6542ec4808d1b8d5017dea981
santhoshk-github <santhoshmecmca2013@gmail.com>
2024-08-02 19:13:11 +0530

    Update schemas.py
    new column added in schemas script

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,19 @@
+# usr/bin/python
 from pydantic import BaseModel
+
+### Base class
 class PatientBase(BaseModel):
     name: str
     age: int
+    date_of_birth: str
     medical_history: str
+    lab_results: str
 
+### Create class
 class PatientCreate(PatientBase):
     pass
 
+### orm class
 class Patient(PatientBase):
     id: int
 
